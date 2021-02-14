e353e165f5ab3901dbf2dcc464e1cdf4d8371876
TondeMoon <tanek8981@gmail.com>
2021-02-15 00:05:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React,  {useState} from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const  items = [
+    {
+        title: "Whats is React?",
+        content: "React is a front end Javascript framework"
+    },
+    {
+        title: "Why use React?",
+        content: "React is a favourite JS library among engineers"
+    },
+    {
+        title: "How to use React?",
+        content: "We use React to create our applications"
+    }
+];
 
-export default App;
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Shade of Blue',
+        value: 'blue'
+    },
+
+];
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    const [selected, setSelected] = useState(options[0])
+
+    return (
+        <div> 
+        <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                 label="Select a color"
+                 options={options} 
+                 selected={selected} 
+                 onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
+        </div>
+    );
+};
